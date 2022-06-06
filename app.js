3729126850674f8a1a7d0d98fd3c0d1894fbf354
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-06 10:54:55 -0500

    Setup error message
    An error message pops up on the console when the user enters the
    wrong input string.

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ function computerPlay () {
 
 function playerSelection () {
     var playerChoice = prompt("Choose rock, paper, or scissors");
-    return playerChoice;
+    return errorPlayerChoice(capitalizeFirstLetter(playerChoice));
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  }
+
+function errorPlayerChoice(string) {
+    if (string == choices[0] || string == choices[1] || string == choices[2]) {
+        return string;
+    }
+    else {
+        var errorMessage = "ERROR: That is not a choice. Please select rock, paper, or scissors."
+        return errorMessage
+    }
 }
 
 console.log(computerPlay());
